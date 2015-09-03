e987b686ff044a437b38f9dd75dd61235b31d0c2
dusan91judo <dusan91judo@gmail.com>
2015-09-03 16:09:39 +0200

    included assets

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,17 +5,30 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", name="home")
      */
-    public function indexAction(Request $request)
+    public function homeAction(Request $request)
+    {
+        return $this->render(
+            'AppBundle:index:index.html.twig'
+        );
+    }
+
+    /**
+     * @Route("/1", name="homepage")
+     */
+    public function indexAction()
     {
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', array(
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
         ));
     }
+
+
 }
---
+++ src/AppBundle/Resources/public/js/script.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+    alert("Hopa cupa");
+console.log("jebo vas brondbi");
+});
